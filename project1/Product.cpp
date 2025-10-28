fe1059ab0073dc97164bf1f924963b61601d14c5
ronykahana <35880798+ronykahana@users.noreply.github.com>
2025-10-28 18:51:40 +0000

    Committing all changes

---
+++ project1/Product.cpp
@@ -0,0 +1,52 @@
+/*
+ * Vending Machine Project
+ * CSE-40477
+ *
+ * Product.cpp
+ * YOU MUST IMPLEMENT THE FUNCTIONS IN THIS FILE.
+ */
+#include <cstring>
+using std::strcpy;
+
+#include "Product.h"
+
+Project1::Product::Product(const char *brand, const char *name,
+                           const char *size) {
+
+  // char *strcpy(char brand, const char *brand);
+  strcpy(this->brand, brand);
+  strcpy(this->name, name);
+  strcpy(this->size, size);
+
+  // TODO: Implement
+}
+
+const char *Project1::Product::getBrand() const {
+  // TODO: Implement
+  return brand;
+}
+
+void Project1::Product::setBrand(const char *value) {
+  // TODO: Implement
+  strcpy(brand, value);
+}
+
+const char *Project1::Product::getName() const {
+  // TODO: Implement
+  return name;
+}
+
+void Project1::Product::setName(const char *value) {
+  // TODO: Implement
+  strcpy(name, value);
+}
+
+const char *Project1::Product::getSize() const {
+  // TODO: Implement
+  return size;
+}
+
+void Project1::Product::setSize(const char *value) {
+  // TODO: Implement
+  strcpy(size, value);
+}
