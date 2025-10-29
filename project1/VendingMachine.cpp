42192d37740ad99d101d57a1f3251e1a8ebafaae
ronykahana <35880798+ronykahana@users.noreply.github.com>
2025-10-29 02:11:35 +0000

    Initial implement vending machine

--- project1/VendingMachine.cpp
+++ project1/VendingMachine.cpp
@@ -17,16 +17,42 @@ Project1::VendingMachine::VendingMachine(
         unspentMoneyCents(0)
 {
     // TODO: Implement
+    
 }
 
 Project1::VendingMachine::~VendingMachine()
 {
-    // TODO: Implement
+    Coin::~Coin;
+    DeliveryChute::~DeliveryChute;
+    ProductButton::~ProductButton;
+    Product::~Product;
+    ProductRack::~ProductRack;
+    StatusPanel::~StatusPanel;
+    delete VendingMachine;
 }
 
 bool
 Project1::VendingMachine::insertCoin(Coin *pCoin)
 {
+    Coin::Coin c(*pCoin);
+    if(c.getDenomination() == COINTYPE_WOODEN_NICKEL){
+        this-> statusPanel.displayMessage(MESSAGE_CODE_BAD_COIN);
+        return false;
+    }else{
+        this->coinBox.push(c);
+        this->numCoins = this->coinBox.size();
+        cout<<"Updated numCoins to "<<this->numCoins<<endl;
+        this->unspentMoneyCents += c.getValueCents();
+        cout<<"Updated unspent cents to: "<<this->unspentMoneyCents<<endl;
+
+        return true;
+    }
+   // Inserts a coin into the vending machine coin slot; this
+        //      increases the current balance that can be used to purchase
+        //      products.  If the coin is invalid (a wooden nickel) the coin
+        //      will be rejected and MESSAGE_CODE_BAD_COIN will be output to
+        //      the status panel.  It is safe to assume that no more than
+        //      MAX_COINS will ever be inserted into the vending machine.
     // TODO: Implement
     return false;
 }
